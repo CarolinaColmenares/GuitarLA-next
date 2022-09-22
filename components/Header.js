61f6b1893e05f2c1e8d3990260fc7f6761c85249
CarolinaColmenares <yzcarcolmenares@gmail.com>
2022-09-21 23:58:02 -0300

    página Nosotros

--- components/Header.js
+++ components/Header.js
@@ -11,7 +11,7 @@ const Header = () => {
                         <Image width={400} height={100} src="/img/logo.svg" alt="Imagen logo" />
                     </Link>
                     <nav className={styles.navegacion}>
-                        <Link href="/">Inicio</Link>
+                        <Link href="/">Principal</Link>
                         <Link href="/nosotros">Nosotros</Link>
                         <Link href="/blog">Blog</Link>
                         <Link href="/tienda">Tienda</Link>
